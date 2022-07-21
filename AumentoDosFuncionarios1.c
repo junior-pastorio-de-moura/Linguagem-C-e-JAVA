8c38a43234e38c1e387f29748624715eae559d4e
Júnior Pastório de Moura <74156231+junior-pastorio-de-moura@users.noreply.github.com>
2022-07-21 10:53:19 -0300

    Add files via upload

---
+++ AumentoDosFuncionarios1.c
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<locale.h>
+#include<math.h>
+int main(){
+    setlocale(LC_ALL, "portuguese");
+    int sal, aum, x;
+    printf("ORGANIZAÇÕS TABAJARA\n\n");
+    printf("Digite o salário do colaborador: ");
+    scanf("%i", &sal);
+    if(sal<=880){
+        aum = sal*0.2;
+        printf("Aumento de 20%%!\n");
+    } else if(sal>880 && sal<=1500){
+        aum = sal*0.15;
+        printf("Aumento de 15%%!\n");
+    } else if(sal>1500 && sal<=3000){
+        aum = sal*0.1;
+        printf("Aumento de 10%%!\n");
+    } else{
+        aum = sal*0.05;
+        printf("Aumento de 5%%!\n");
+    }
+    printf("Percentual de aumento aplicado: %R$;%i,00\n", aum);
+    x = sal + aum;
+    printf("Novo salário: R$;%i,00\n", x);
+    system("pause");
+    return 0;
+}
