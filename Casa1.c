0af22707a3b282c078a6a0b97d1e78d7a60a8e51
Júnior Pastório de Moura <74156231+junior-pastorio-de-moura@users.noreply.github.com>
2022-08-18 21:21:58 -0300

    Add files via upload

---
+++ Casa1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("%c%c%c%c\n", '#', '#', '#', '#');
+    printf("%c%c%c%c\n", '#', '#', '#', '#');
+    printf("%c%c%c%c%c%c%c\n", '#', '#', '#', '#', '#', '#', '#');
+    printf("%c%c%c%c%c%c%c\n", '#', '#', '#', '#', '#', '#', '#');
+    printf("%c%c%c%c\n", '#', '#', '#', '#');
+    printf("%c%c%c%c\n", '#', '#', '#', '#');
+
+    system("pause");
+    return 0;
+}
