b52baf90cead095e27a3b26a4cc47efaac0aed84
Júnior Pastório de Moura <74156231+junior-pastorio-de-moura@users.noreply.github.com>
2022-08-19 11:29:14 -0300

    Add files via upload

---
+++ ComparacaoDeValores.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>    
+#include <locale.h>
+#include<conio.h>
+#include<time.h>
+
+
+int main(){
+    int x[3];
+    printf("Digite 3 valores: ");
+    scanf("%d %d %d", &x[1], &x[2], &x[3]);
+    while ()
+    {
+        compara = x[1];
+    }
+    
+
+    system("pause");   
+    return 0;
+}
+/*
+Criar um codigo que compare n valores, dizendo se eles são iguais ou diferentes,
+e dando a opção de colocá-los em ordem crescente ou decrescente.
+
+Criar um código que coloque n valores em ordem crescente ou decrescente, tendo essa opção.
+*/
