a00ea982af7355df8ed35191a9efd9b2ea4b7e83
Júnior Pastório de Moura <74156231+junior-pastorio-de-moura@users.noreply.github.com>
2022-09-02 21:48:59 -0300

    Add files via upload

---
+++ ExtremamenteBasico.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+    int a, b, x;
+    scanf("%d %d", &a, &b);
+    x = a+b;
+    printf("x = %d\n", x);
+    system("pause");
+    return 0;
+}
