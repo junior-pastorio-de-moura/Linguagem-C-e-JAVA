7ec5503e6d80a946a69f5fc42d87fe274f28e6e4
Júnior Pastório de Moura <74156231+junior-pastorio-de-moura@users.noreply.github.com>
2023-02-06 22:29:38 -0300

    Add files via upload

---
+++ escopo3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int x = 10;
+
+int main (void){ 
+    printf("Valor de x = %d\n", x);
+    int x = -1;
+    printf("Valor de x = %d\n", x);
+    return 0;
+}
