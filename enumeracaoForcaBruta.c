325ca7fac44ac1053080ce75be4a69c703a03afb
Júnior Pastório de Moura <74156231+junior-pastorio-de-moura@users.noreply.github.com>
2023-03-16 07:44:43 -0300

    Add files via upload

---
+++ enumeracaoForcaBruta.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<assert.h>
+#include<string.h>
+#include<math.h>
+//Programa para enumeração simples de bandeiras com duas cores
+#define max 100
+
+//Função apra exibição de uma solução
+void exiba(int v[], int n, char *map[]){
+    printf("[");
+    for(int j = 0; j<n; j++){
+        printf("%s", map[v[j]]);
+        if(j<n-1){
+            printf(",");
+        }
+    }
+    puts("]");
+}
+
+//Função para verificação de restrições em folhas
+int solucao(int v[], int i){
+    return (v[1] != v[0]);
+}
+
+//Função de enumeração por força bruta
+void enumeref(int n, char *map[], int *dom[], int m){
+    static int v[max], i = -1;
+    if(i==n-1){
+        if(solucao(v,i)){
+            exiba(v, i+1, map);
+        } else{
+            for(int *d = dom[(i+1)%m]; *d>=0; d++){
+                v[++i] = *;
+                enumeref(n,map,dom,m);
+                i--;
+            }
+        }
+    }
+}
+
+int main(void){
+    char *map[] = {"azul", "verde", "amarelo"};
+    int D0[] = {0, 1, 2, -1};
+    int *dom[] = {D0};
+    enumeref(2, map, dom, 1);
+    return 0;
+}
+
+/*
+    char *map[] = {"pao", "salada", "peixe", "carne", "frango", "fruta", "pudim"};
+    int D0[] = {0, 1, -1};
+    int D1[] = {2, 3, 4, -1};
+    int D2[] = {5, 6, -1};
+
+    int *dom[] = {D0, D1, D2};
+    */
